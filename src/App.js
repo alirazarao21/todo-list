8f27277ea881275207fc3e4c81490e37144cf485
unknown <raoa62253@gmail.com>
2023-08-06 08:17:20 -0700

    starting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+const app = ()=> {
+  return (
+    <>
+    <div>
+      <img src={required("E:\pics\198.jpg")} alt="pic"></img>
+    </div>
+    <h1>Hello, how are you</h1>
+  <h1>i am ali</h1>
+  </>
+  )
+
+}
+ export default app;
